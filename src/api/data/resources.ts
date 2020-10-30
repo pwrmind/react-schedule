88b380414ff53cf049c02e4d4d6dfb3c6d5d3fc8
qAlter <DZuev2@nvg.ru>
2020-10-30 09:52:17 +0300

    specialty in data

--- src/api/data/resources.ts
+++ src/api/data/resources.ts
@@ -1,6 +1,7 @@
 export interface Resource {
 	id: number;
 	name: string;
+	specialty: string;
 	workPlace: string;
 	roomNumber: number;
 	description: string;
@@ -9,35 +10,40 @@ export interface Resource {
 const resourceList: Array<Resource> = [
 	{
 		id: 0,
-		name: 'Григорьева Г.Г. Терапевт',
+		name: 'Григорьева Г.Г.',
+		specialty: 'Терапевт',
 		workPlace: 'ГП №128',
 		roomNumber: 110,
 		description: 'description'
 	},
 	{
 		id: 1,
-		name: 'Григорьева Г.Г. Терапевт',
+		name: 'Сидорова С.С.',
+		specialty: 'Терапевт',
 		workPlace: 'ГП №128',
 		roomNumber: 120,
 		description: 'description'
 	},
 	{
 		id: 2,
-		name: 'Григорьева Г.Г. Терапевт',
+		name: 'Сидорова С.С.',
+		specialty: 'Терапевт',
 		workPlace: 'ГП №128',
 		roomNumber: 130,
 		description: 'description'
 	},
 	{
 		id: 3,
-		name: 'Григорьева Г.Г. Терапевт',
+		name: 'Елисеева Е.Е.',
+		specialty: 'Офтальмолог',
 		workPlace: 'ГП №128',
 		roomNumber: 140,
 		description: 'description'
 	},
 	{
 		id: 4,
-		name: 'Григорьева Г.Г. Терапевт',
+		name: 'Константинова-Щедрина А.А.',
+		specialty: 'Офтальмолог',
 		workPlace: 'ГП №128',
 		roomNumber: 150,
 		description: 'description'
